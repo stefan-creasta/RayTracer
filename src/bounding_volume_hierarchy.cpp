b0d33ceff575c5b6e7f86b34d33376e98af20779
Bora İnevi <m.b.inevi@student.tudelft.nl>
2022-10-24 02:30:22 +0200

    reimplement according to specification

--- src/bounding_volume_hierarchy.cpp
+++ src/bounding_volume_hierarchy.cpp
@@ -6,7 +6,8 @@
 #include "interpolate.h"
 #include <glm/glm.hpp>
 
-void calculateCentroid(MeshTrianglePair& meshTrianglePair) { 
+void calculateCentroid(MeshTrianglePair& meshTrianglePair)
+{
     std::vector<glm::uvec3>& triangles = meshTrianglePair.mesh->triangles;
     std::vector<Vertex>& vertices = meshTrianglePair.mesh->vertices;
     int x = triangles[meshTrianglePair.triangle].x;
@@ -18,7 +19,8 @@ void calculateCentroid(MeshTrianglePair& meshTrianglePair) {
     meshTrianglePair.centroid = (1.f / 3.f) * (p0 + p1 + p2);
 }
 
-AxisAlignedBox getTriangleAAB(const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2) { 
+AxisAlignedBox getTriangleAAB(const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2)
+{
     return AxisAlignedBox {
         glm::vec3 {
             std::min({ p0.x, p1.x, p2.x }),
@@ -33,7 +35,8 @@ AxisAlignedBox getTriangleAAB(const glm::vec3& p0, const glm::vec3& p1, const gl
     };
 }
 
-AxisAlignedBox getTriangleAAB(const MeshTrianglePair& meshTrianglePair) { 
+AxisAlignedBox getTriangleAAB(const MeshTrianglePair& meshTrianglePair)
+{
     std::vector<glm::uvec3>& triangles = meshTrianglePair.mesh->triangles;
     std::vector<Vertex>& vertices = meshTrianglePair.mesh->vertices;
     int x = triangles[meshTrianglePair.triangle].x;
@@ -45,82 +48,54 @@ AxisAlignedBox getTriangleAAB(const MeshTrianglePair& meshTrianglePair) {
     return getTriangleAAB(p0, p1, p2);
 }
 
-AxisAlignedBox mergeAABs(const AxisAlignedBox& A, const AxisAlignedBox& B) { 
+AxisAlignedBox mergeAABs(const AxisAlignedBox& A, const AxisAlignedBox& B)
+{
     return AxisAlignedBox {
         glm::min(glm::min(A.lower, B.lower), glm::min(A.upper, B.upper)),
         glm::max(glm::max(A.lower, B.lower), glm::max(A.upper, B.upper)),
     };
 }
 
-size_t getMedian(const std::span<size_t>& indices, const std::span<MeshTrianglePair>& meshTrianglePairs, int direction) { 
+size_t getMedian(const std::span<size_t>& indices, const std::span<MeshTrianglePair>& meshTrianglePairs, int direction)
+{
     std::sort(indices.begin(), indices.end(), [meshTrianglePairs, direction](const size_t a, const size_t b) {
         return (
-            (direction == 0) && (meshTrianglePairs[a].centroid.x > meshTrianglePairs[b].centroid.x) ||
-            (direction == 1) && (meshTrianglePairs[a].centroid.y > meshTrianglePairs[b].centroid.y) ||
-            (direction == 2) && (meshTrianglePairs[a].centroid.z > meshTrianglePairs[b].centroid.z)
-        );
+            (direction == 0) && (meshTrianglePairs[a].centroid.x > meshTrianglePairs[b].centroid.x) || (direction == 1) && (meshTrianglePairs[a].centroid.y > meshTrianglePairs[b].centroid.y) || (direction == 2) && (meshTrianglePairs[a].centroid.z > meshTrianglePairs[b].centroid.z));
     });
     return indices.size() / 2;
 }
 
-BoundingVolumeHierarchy BoundingVolumeHierarchy::bvhSplitHelper(Scene* pScene, std::vector<BoundingVolumeHierarchy>& allNodes, const std::span<size_t>& indices, const std::span<MeshTrianglePair>& meshTrianglePairs, int direction)
+size_t bvhSplitHelper(Scene* pScene, std::vector<Node>& nodes, const std::span<size_t>& indices, const std::span<MeshTrianglePair>& meshTrianglePairs, int direction, int currentDepth, int maxDepth = -1)
 {
-    if (indices.size() == 0) {
-        BoundingVolumeHierarchy current = BoundingVolumeHierarchy(pScene, std::optional<MeshTrianglePair>(), AxisAlignedBox {});
-        current.m_isEmpty = true;
-        return current;
+    Node result;
+
+    if (maxDepth > -1 && maxDepth <= currentDepth || indices.size() < 2) {
+        AxisAlignedBox aab = getTriangleAAB(meshTrianglePairs[indices[0]]);
+
+        for (size_t i : indices) {
+            aab = mergeAABs(aab, getTriangleAAB(meshTrianglePairs[i]));
+        }
+
+        nodes.push_back(Node { std::vector<size_t>(indices.begin(), indices.end()), aab, true, currentDepth, 1, 1 });
+        return nodes.size() - 1;
     }
-    if (indices.size() == 1) {
-        MeshTrianglePair pair = meshTrianglePairs[indices[0]];
-        const auto aab = getTriangleAAB(pair);
-        BoundingVolumeHierarchy current = BoundingVolumeHierarchy(pScene, pair, aab);
-        current.m_isEmpty = false;
-        return current;
-    }
+
     size_t median = getMedian(indices, meshTrianglePairs, direction);
     const std::span<size_t> leftIndices = std::span<size_t>(indices.begin(), indices.begin() + median);
     const std::span<size_t> rightIndices = std::span<size_t>(indices.begin() + median, indices.end());
 
-    BoundingVolumeHierarchy hLeft = bvhSplitHelper(pScene, allNodes, leftIndices, meshTrianglePairs, (direction + 1) % 3);
-    BoundingVolumeHierarchy hRight = bvhSplitHelper(pScene, allNodes, rightIndices, meshTrianglePairs, (direction + 1) % 3);
+    size_t left = bvhSplitHelper(pScene, nodes, leftIndices, meshTrianglePairs, (direction + 1) % 3, currentDepth + 1, maxDepth);
+    size_t right = bvhSplitHelper(pScene, nodes, rightIndices, meshTrianglePairs, (direction + 1) % 3, currentDepth + 1, maxDepth);
 
-    AxisAlignedBox currentBox = mergeAABs(hLeft.axisAlignedBox, hRight.axisAlignedBox);
-
-    allNodes.push_back(hLeft);
-    allNodes.push_back(hRight);
-
-    BoundingVolumeHierarchy current = BoundingVolumeHierarchy(pScene, hLeft, hRight, currentBox);
-    current.m_isEmpty = false;
-    
-    return current;
-}
-
-// Constructor. Used to create a leaf node or an empty BoundingVolumeHierarchy.
-BoundingVolumeHierarchy::BoundingVolumeHierarchy(Scene* pScene, const std::optional<MeshTrianglePair>& meshTrianglePair, const AxisAlignedBox axisAlignedBox)
-    : m_pScene(pScene)
-    , meshTrianglePair(meshTrianglePair)
-    , axisAlignedBox(axisAlignedBox)
-    , m_numLeaves(1)
-    , m_numLevels(1)
-    , m_isLeaf(true)
-    , m_isEmpty(false)
-    , children({})
-{ 
-}
-
-// Constructor. Used to create the inner nodes of the BoundingVolumeHierarchy tree.
-BoundingVolumeHierarchy::BoundingVolumeHierarchy(Scene* pScene, BoundingVolumeHierarchy& hLeft, BoundingVolumeHierarchy& hRight, const AxisAlignedBox axisAlignedBox)
-    : m_pScene(pScene)
-    , meshTrianglePair(meshTrianglePair)
-    , axisAlignedBox(axisAlignedBox)
-    , m_isLeaf(false)
-    , m_isEmpty(false)
-    , children({})
-{
-    this->m_numLeaves = hLeft.m_numLeaves + hRight.m_numLeaves;
-    this->m_numLevels = glm::max(hLeft.m_numLevels, hRight.m_numLevels) + 1;
-    children.push_back(hLeft);
-    children.push_back(hRight);
+    nodes.push_back(Node { 
+        { left, right }, 
+        mergeAABs(nodes[left].axisAlignedBox, nodes[right].axisAlignedBox), 
+        false, 
+        currentDepth,
+        glm::max(nodes[left].numLevels, nodes[right].numLevels) + 1, 
+        nodes[left].numLeaves + nodes[right].numLeaves 
+    });
+    return nodes.size() - 1;
 }
 
 BoundingVolumeHierarchy::BoundingVolumeHierarchy(Scene* pScene)
@@ -131,20 +106,20 @@ BoundingVolumeHierarchy::BoundingVolumeHierarchy(Scene* pScene)
         n += mesh.triangles.size();
     }
 
-    allNodes = std::vector<BoundingVolumeHierarchy>();
+    nodes = std::vector<Node>();
 
     if (n == 0) {
-        new (this) BoundingVolumeHierarchy(pScene, std::optional<MeshTrianglePair>(), AxisAlignedBox {});
-        allNodes.push_back(*this);
-        this->m_isEmpty = true;
+        root = -1;
+        m_numLevels = 0;
+        m_numLeaves = 0;
         return;
     }
 
     std::vector<size_t> indices = std::vector<size_t>();
-    std::vector<MeshTrianglePair> meshTrianglePairs = std::vector<MeshTrianglePair>();
+    meshTrianglePairs = std::vector<MeshTrianglePair>();
     indices.reserve(n);
     meshTrianglePairs.reserve(n);
-    allNodes.reserve(n);
+    nodes.reserve(2 * n - 1);
 
     // i is incremented in the inner loop!
     size_t i = 0;
@@ -159,16 +134,9 @@ BoundingVolumeHierarchy::BoundingVolumeHierarchy(Scene* pScene)
         }
     }
 
-    BoundingVolumeHierarchy bvh = bvhSplitHelper(pScene, allNodes, indices, meshTrianglePairs, 0);
-
-    this->m_numLevels = bvh.m_numLevels;
-    m_numLeaves = bvh.m_numLeaves;
-    m_isLeaf = bvh.m_isLeaf;
-    m_isEmpty = bvh.m_isEmpty;
-    m_pScene = pScene;
-    meshTrianglePair = bvh.meshTrianglePair;
-    axisAlignedBox = bvh.axisAlignedBox;
-    children = bvh.children;
+    root = bvhSplitHelper(pScene, nodes, indices, meshTrianglePairs, 0, 0);
+    m_numLevels = nodes[root].numLevels;
+    m_numLeaves = nodes[root].numLeaves;
 }
 
 // Return the depth of the tree that you constructed. This is used to tell the
@@ -185,31 +153,20 @@ int BoundingVolumeHierarchy::numLeaves() const
     return this->m_numLeaves;
 }
 
-// Return if the node is a leaf.
-bool BoundingVolumeHierarchy::isLeaf() const
+void debugDrawLevelHelper(std::vector<Node>& nodes, size_t node, int level)
 {
-    return this->m_isLeaf;
-}
-
-// Return if the tree doesn't contain any bounding volumes to traverse.
-bool BoundingVolumeHierarchy::isEmpty() const
-{
-    return this->m_isEmpty;
-}
-
-void BoundingVolumeHierarchy::debugDrawLevelHelper(int totalDepth, int level) {
     // Draw the AABB as a transparent green box.
     // AxisAlignedBox aabb{ glm::vec3(-0.05f), glm::vec3(0.05f, 1.05f, 1.05f) };
     // drawShape(aabb, DrawMode::Filled, glm::vec3(0.0f, 1.0f, 0.0f), 0.2f);
 
     // Draw the AABB as a (white) wireframe box.
-    if (totalDepth - this->numLevels() >= level) {
-        drawAABB(axisAlignedBox, DrawMode::Filled, glm::vec3(0.05f, 1.0f, 0.05f), 0.1f);
+    if (nodes[node].depth == level) {
+        drawAABB(nodes[node].axisAlignedBox, DrawMode::Filled, glm::vec3(0.05f, 1.0f, 0.05f), 0.1f);
     } else {
-        if (!this->isLeaf()) {
+        if (!nodes[node].isLeaf) {
             // Adjust total depth by how much each subtree differs in height from the one with greater height.
-            children[0].debugDrawLevelHelper(totalDepth - (this->numLevels() - 1 - children[0].numLevels()), level);
-            children[1].debugDrawLevelHelper(totalDepth - (this->numLevels() - 1 - children[1].numLevels()), level);
+            debugDrawLevelHelper(nodes, nodes[node].children[0], level);
+            debugDrawLevelHelper(nodes, nodes[node].children[1], level);
         }
     }
     // drawAABB(aabb, DrawMode::Wireframe);
@@ -219,9 +176,11 @@ void BoundingVolumeHierarchy::debugDrawLevelHelper(int totalDepth, int level) {
 // mode, arbitrary colors and transparency.
 void BoundingVolumeHierarchy::debugDrawLevel(int level)
 {
-    debugDrawLevelHelper(this->numLevels(), level);
-}
+    if (root == -1)
+        return;
 
+    debugDrawLevelHelper(nodes, root, level);
+}
 
 // Use this function to visualize your leaf nodes. This is useful for debugging. The function
 // receives the leaf node to be draw (think of the ith leaf node). Draw the AABB of the leaf node and all contained triangles.
@@ -230,24 +189,30 @@ void BoundingVolumeHierarchy::debugDrawLevel(int level)
 void BoundingVolumeHierarchy::debugDrawLeaf(int leafIdx)
 {
     // Draw the AABB as a transparent green box.
-    //AxisAlignedBox aabb{ glm::vec3(-0.05f), glm::vec3(0.05f, 1.05f, 1.05f) };
-    //drawShape(aabb, DrawMode::Filled, glm::vec3(0.0f, 1.0f, 0.0f), 0.2f);
+    // AxisAlignedBox aabb{ glm::vec3(-0.05f), glm::vec3(0.05f, 1.05f, 1.05f) };
+    // drawShape(aabb, DrawMode::Filled, glm::vec3(0.0f, 1.0f, 0.0f), 0.2f);
 
     // Draw the AABB as a (white) wireframe box.
-    if (isLeaf()) {
-        drawAABB(axisAlignedBox, DrawMode::Filled, glm::vec3(0.05f, 1.0f, 0.05f), 0.1f);
-    } else {
-        if (children[0].numLeaves() > leafIdx)
-            children[0].debugDrawLeaf(leafIdx);
-        else
-            children[1].debugDrawLeaf(leafIdx - children[0].numLeaves());
+    if (root == -1)
+        return;
+
+    Node current = nodes[root];
+    int currentLeafIdx = leafIdx;
+
+    while (!current.isLeaf) { 
+        if (nodes[current.children[0]].numLeaves > currentLeafIdx) {
+            current = nodes[current.children[0]];
+        } else {
+            currentLeafIdx -= nodes[current.children[0]].numLeaves;
+            current = nodes[current.children[1]];
+        }
     }
-    //drawAABB(aabb, DrawMode::Wireframe);
-    
+
+    drawAABB(current.axisAlignedBox, DrawMode::Filled, glm::vec3(0.05f, 1.0f, 0.05f), 0.1f);
+    // drawAABB(aabb, DrawMode::Wireframe);
 
     // once you find the leaf node, you can use the function drawTriangle (from draw.h) to draw the contained primitives
 }
-
 
 // Return true if something is hit, returns false otherwise. Only find hits if they are closer than t stored
 // in the ray and if the intersection is on the correct side of the origin (the new t >= 0). Replace the code
--- src/bounding_volume_hierarchy.h
+++ src/bounding_volume_hierarchy.h
@@ -6,10 +6,20 @@
 
 // Forward declaration.
 struct Scene;
+
 struct MeshTrianglePair {
     Mesh* mesh;
     int triangle;
     glm::vec3 centroid;
+};
+
+struct Node {
+    std::vector<size_t> children;
+    AxisAlignedBox axisAlignedBox;
+    bool isLeaf;
+    int depth;
+    int numLevels;
+    int numLeaves;
 };
 
 class BoundingVolumeHierarchy {
@@ -17,27 +27,14 @@ public:
     // Constructor. Receives the scene and builds the bounding volume hierarchy.
     BoundingVolumeHierarchy(Scene* pScene);
 
-    // Constructor. Used to create a leaf node or an empty BoundingVolumeHierarchy.
-    BoundingVolumeHierarchy(Scene* pScene, const std::optional<MeshTrianglePair>& meshTrianglePair, const AxisAlignedBox axisAlignedBox);
-
-    // Constructor. Used to create the inner nodes of the BoundingVolumeHierarchy tree.
-    BoundingVolumeHierarchy(Scene* pScene, BoundingVolumeHierarchy& hLeft, BoundingVolumeHierarchy& hRight, const AxisAlignedBox axisAlignedBox);
-
     // Return how many levels there are in the tree that you have constructed.
     [[nodiscard]] int numLevels() const;
 
     // Return how many leaf nodes there are in the tree that you have constructed.
     [[nodiscard]] int numLeaves() const;
 
-    // Return true iff the node is a leaf node.
-    [[nodiscard]] bool isLeaf() const;
-
-    // Return if the tree doesn't contain any bounding volumes to traverse.
-    [[nodiscard]] bool isEmpty() const;
-
     // Visual Debug 1: Draw the bounding boxes of the nodes at the selected level.
     void debugDrawLevel(int level);
-    void debugDrawLevelHelper(int totalDepth, int level);
 
     // Visual Debug 2: Draw the triangles of the i-th leaf
     void debugDrawLeaf(int leafIdx);
@@ -47,17 +44,12 @@ public:
     // is on the correct side of the origin (the new t >= 0).
     bool intersect(Ray& ray, HitInfo& hitInfo, const Features& features) const;
 
-    static BoundingVolumeHierarchy bvhSplitHelper(Scene* pScene, std::vector<BoundingVolumeHierarchy>& allNodes, const std::span<size_t>& indices, const std::span<MeshTrianglePair>& meshTrianglePairs, int direction);
-
 
 private:
     int m_numLevels;
     int m_numLeaves;
-    bool m_isLeaf;
-    bool m_isEmpty;
     Scene* m_pScene;
-    std::optional<MeshTrianglePair> meshTrianglePair;
-    AxisAlignedBox axisAlignedBox;
-    std::vector<BoundingVolumeHierarchy> children;
-    std::vector<BoundingVolumeHierarchy> allNodes;
+    size_t root;
+    std::vector<Node> nodes;
+    std::vector<MeshTrianglePair> meshTrianglePairs;
 };
