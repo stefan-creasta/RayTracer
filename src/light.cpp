89f73be48155d8672cf99cbcf64d0945ef9323e1
Stefan Creasta <Creasta@student.tudelft.nl>
2022-10-24 22:41:06 +0200

    Adding area lights and debug for them
    The debug for area lights might not be final

--- src/light.cpp
+++ src/light.cpp
@@ -9,35 +9,38 @@ DISABLE_WARNINGS_POP()
 #include <random>
 bool flag = false; // debug random light samples
 #include <iostream>
+glm::vec3 posForDebug = glm::vec3(-10000.0f);
+std::vector<glm::vec3> prevPos;
+std::vector<glm::vec3> prevCol;
 
-float getRandomVal2()
+
+float getRandomVal()
 {
     std::random_device rd;
     std::mt19937 generator(rd());
     std::uniform_real_distribution<float> distribution(0.0, 1.0);
     float randomVal = distribution(generator);
-    float randomVal2 = distribution(generator);
-    //std::cout << randomVal << randomVal2 << std::endl;
     return randomVal;
 }
-
 // samples a segment light source
 // you should fill in the vectors position and color with the sampled position and color
 void sampleSegmentLight(const SegmentLight& segmentLight, glm::vec3& position, glm::vec3& color)
 {
-    position = glm::vec3(0.0);
-    color = glm::vec3(0.0);
-    // TODO: implement this function.
+    glm::vec3 p0ToP1 = segmentLight.endpoint1 - segmentLight.endpoint0;
+    float alpha = getRandomVal();
+    position = alpha * p0ToP1 + segmentLight.endpoint0;
+    color = (1 - alpha) * segmentLight.color0 + alpha * segmentLight.color1;
 }
 
 // samples a parallelogram light source
 // you should fill in the vectors position and color with the sampled position and color
 void sampleParallelogramLight(const ParallelogramLight& parallelogramLight, glm::vec3& position, glm::vec3& color)
 {
-    float alpha1 = getRandomVal2();
+    // first v0 to v1
+    float alpha1 = getRandomVal();
     glm::vec3 c1 = (1 - alpha1) * parallelogramLight.color0 + alpha1 * parallelogramLight.color1;
     glm::vec3 c2 = (1 - alpha1) * parallelogramLight.color2 + alpha1 * parallelogramLight.color3;
-    float alpha2 = getRandomVal2();
+    float alpha2 = getRandomVal();
     color = (1 - alpha2) * c1 + alpha2 * c2;
     position = parallelogramLight.v0 + parallelogramLight.edge01 * alpha1 + parallelogramLight.edge02 * alpha2;
 }
@@ -62,17 +65,17 @@ float testVisibilityLightSample(const glm::vec3& samplePos, const glm::vec3& deb
     glm::vec3 secondHit = sray.origin + sray.t * sray.direction;
     if (glm::dot(glm::normalize(samplePos - hit), glm::normalize(normal)) < -eps) {
 
-        if ((features.enableHardShadow || features.enableSoftShadow) && !flag)
+        if ((features.enableHardShadow || features.enableSoftShadow))
             drawRay(sray, glm::vec3{1, 0, 0});
         return 0.0f;
     }
     if (glm::distance(hit, secondHit) > 1e-3) {
-        if ((features.enableHardShadow || features.enableSoftShadow) && !flag)
+        if ((features.enableHardShadow || features.enableSoftShadow))
             drawRay(sray, glm::vec3{1, 0, 0});
         return 0.0f;
     }
 
-    if ((features.enableHardShadow || features.enableSoftShadow) && !flag)
+    if ((features.enableHardShadow || features.enableSoftShadow))
         drawRay(sray, debugColor);
     
     return 1.0f;
@@ -129,17 +132,60 @@ glm::vec3 computeLightContribution(const Scene& scene, const BvhInterface& bvh, 
             } else if (std::holds_alternative<SegmentLight>(light)) {
                 const SegmentLight segmentLight = std::get<SegmentLight>(light);
                 // Perform your calculations for a segment light.
-            } else if (std::holds_alternative<ParallelogramLight>(light)) {
-                const ParallelogramLight parallelogramLight = std::get<ParallelogramLight>(light);
-                // Perform your calculations for a parallelogram light.
                 if (features.enableSoftShadow) {
-                    std::cout << "8";
-                    int sampleSize = 500;
+                    int sampleSize = 50;
                     glm::vec3 avgColor = { 0.0f, 0.0f, 0.0f };
+                    //DEBUG FOR SEGMENT LIGHT
+                    glm::vec3 pos = ray.t * ray.direction + ray.origin;
+                    float eps = 1e-6;
+                    if (std::fabs(posForDebug.x - pos.x) < eps || std::fabs(posForDebug.y - pos.y) < eps || std::fabs(posForDebug.z - pos.z) < eps) {
+                        flag = false;
+                        prevCol.clear();
+                        prevPos.clear();
+                    }
+                    //DEBUG FOR SEGMENT LIGHT
                     for (int i = 0; i < sampleSize; i++) {
                         glm::vec3 pos;
                         glm::vec3 col;
-                        sampleParallelogramLight(parallelogramLight, pos, col);
+                        if (flag == false) {
+                            sampleSegmentLight(segmentLight, pos, col);
+                            prevPos.push_back(pos);
+                            prevCol.push_back(col);
+                        } else {
+                            pos = prevPos[i];
+                            col = prevCol[i];
+                        }
+                        avgColor += computeShading(pos, col, features, ray, hitInfo) * testVisibilityLightSample(pos, col, bvh, features, ray, hitInfo);
+                    }
+                    flag = true;
+                    med += avgColor * float((1.0 / float(sampleSize)));
+                }
+            } else if (std::holds_alternative<ParallelogramLight>(light)) { 
+                const ParallelogramLight parallelogramLight = std::get<ParallelogramLight>(light);
+                // Perform your calculations for a parallelogram light.
+                if (features.enableSoftShadow) {
+                    int sampleSize = 50;
+                    glm::vec3 avgColor = { 0.0f, 0.0f, 0.0f };
+                    // DEBUG FOR PARALLELOGRAM LIGHT
+                    glm::vec3 pos = ray.t * ray.direction + ray.origin;
+                    float eps = 1e-6;
+                    if (std::fabs(posForDebug.x - pos.x) < eps || std::fabs(posForDebug.y - pos.y) < eps || std::fabs(posForDebug.z - pos.z) < eps) {
+                        flag = false;
+                        prevCol.clear();
+                        prevPos.clear();
+                    }
+                    // DEBUG FOR PARALLELOGRAM LIGHT
+                    for (int i = 0; i < sampleSize; i++) {
+                        glm::vec3 pos;
+                        glm::vec3 col;
+                        if (flag == false) {
+                            sampleParallelogramLight(parallelogramLight, pos, col);
+                            prevPos.push_back(pos);
+                            prevCol.push_back(col);
+                        } else {
+                            pos = prevPos[i];
+                            col = prevCol[i];
+                        }
                         // std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
                         avgColor += computeShading(pos, col, features, ray, hitInfo) * testVisibilityLightSample(pos, col, bvh, features, ray, hitInfo);
                     }
--- src/shading.cpp
+++ src/shading.cpp
@@ -26,7 +26,32 @@ const glm::vec3 computeShading(const glm::vec3& lightPosition, const glm::vec3& 
     return lightColor * hitInfo.material.ks * pow(glm::dot(glm::normalize(hitInfo.normal), h), hitInfo.material.shininess) + lightColor * hitInfo.material.kd * dot;
 }
 
+/** float getRandomVal()
+{
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_real_distribution<float> distribution(0.0, 1.0);
+    float randomVal = distribution(generator);
+    float randomVal2 = distribution(generator);
+    // std::cout << randomVal << randomVal2 << std::endl;
+    return randomVal;
+}
 
+
+Ray returnGlossyRay(Ray reflection)
+{
+    glm::vec3 w = glm::normalize(reflection.direction);
+    glm::vec3 t = glm::normalize(w - glm::vec3 { 0.1f, 0.0f, 0.0f });
+    glm::vec3 u = glm::normalize(glm::cross(t, w));
+    glm::vec3 v = glm::normalize(glm::cross(w, u));
+    float degreeBlur = 0.01f;
+    float ua = -degreeBlur / 2.0f + degreeBlur * getRandomVal();
+    float va = -degreeBlur / 2.0f + degreeBlur * getRandomVal();
+    Ray rr = reflection;
+    rr.direction += ua * u + va * v;
+    return rr;
+}
+**/
 const Ray computeReflectionRay (Ray ray, HitInfo hitInfo)
 {
     glm::vec3 point = ray.origin + ray.direction * ray.t;
@@ -35,4 +60,5 @@ const Ray computeReflectionRay (Ray ray, HitInfo hitInfo)
     Ray reflectionRay {point + float(1e-5)*r, r};
     //drawRay(Ray{point, hitInfo.normal, 1}, glm::vec3{0,1,0.5});
     return reflectionRay;
+    //return returnGlossyRay(reflectionRay);
 }
